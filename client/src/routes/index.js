26a7297bbd826d33ecef13d469171d7d7e960275
douglas-cezaro <cezarodouglas@gmail.com>
2020-08-08 15:38:30 -0300

    Ajustes header bar

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,7 +2,7 @@ import {
   createStackNavigator,
   HeaderBackButton,
 } from "@react-navigation/stack";
-import { Button, Image } from "react-native";
+import { Button, Image, TouchableOpacity } from "react-native";
 import FormCli from "../view/formCliente/index";
 import Form from "../view/form/index";
 import Database from "../database/iniitDatabase";
@@ -12,59 +12,46 @@ import { DrawerActions } from "@react-navigation/native";
 
 const Stack = createStackNavigator();
 
-const MyCustomHeaderBackImage = () => (
-  <Image
-    source={require("../../assets/menu.png")}
-    style={{ width: 40, height: 40, tintColor: "#fff" }}
-  />
-);
-function LogoTitle() {
-  return (
+const button = () => (
+  <TouchableOpacity>
     <Image
+      source={require("../../assets/menu.png")}
       style={{ width: 50, height: 50 }}
-      source={require('../../assets/menu.png')}
     />
-  );
-}
+  </TouchableOpacity>
+);
+
 export default function Routes() {
   return (
     <Stack.Navigator
       initialRouteName="FormCli"
-      // screenOptions={{
-      //   headerStyle: {
-      //     backgroundColor: "#7159c1",
-      //   },
-      //   headerTitleStyle: { alignSelf: "center" },
-      //   headerTintColor: "#ffff",
-
-      // }}
     >
       <Stack.Screen name="Database" component={Database} />
-      <Stack.Screen name="Form" component={Form} />
       <Stack.Screen
         name="FormCli"
         component={MainRoutes}
         options={({ navigation }) => ({
           title: "Volt Orçamento",
-          headerBackImage: MyCustomHeaderBackImage,
-          // headerTitle: props => <LogoTitle {...props} />,
           headerLeft: () => (
-            <HeaderBackButton
-              onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
-             title="Info"
-              
-            />
+            <TouchableOpacity          
+            onPress={() =>
+              navigation.dispatch(DrawerActions.toggleDrawer())
+            }>
+              <Image
+                source={require("../../assets/menu.png")}
+                style={{ width: 50, height: 50 }}
+              />
+            </TouchableOpacity>
           ),
           headerTintColor: "#fff",
           headerStyle: {
-            backgroundColor: "#1790EF",
+            backgroundColor: "#0A7ABF",
             elevation: 0,
             shadowColor: "transparent",
           },
           headerBackTitleVisible: false,
         })}
       />
-          
     </Stack.Navigator>
   );
 }
