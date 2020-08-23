3c806787f878cd56f4a370fd8e616c322561b92c
douglas-cezaro <cezarodouglas@gmail.com>
2020-08-23 15:56:30 -0300

    Ajustes calculos valor final

--- client/src/view/formDatabase/index.js
+++ client/src/view/formDatabase/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView, Alert } from "react-native";
 import InputPattern from "../components/InputPattern/index";
 import ModalIP from "../components/modal/modalIp";
 import MsgModal from "../components/modal/index";
@@ -46,6 +46,12 @@ export default class FormDatabase extends React.Component {
           "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
           "descricao TEXT)"
       );
+
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS calculo_kwp" +
+          "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+          "POTEN1 DOUBLE, POTEN2 DOUBLE, TELHADO INTEGER, SOLO INTEGER, codPadrao INTEGER )"
+      );
     });
     this.state = {
       ip: "",
@@ -57,11 +63,13 @@ export default class FormDatabase extends React.Component {
       tipoRede: [],
       tipoInstall: [],
       Disjuntores: [],
+      CalculoKWP: [],
+      erro: false,
       open: false,
       mensagem: "",
       openIP: false,
       mensagemIP: "Atualizando Tabela X",
-      tipoIcone:"loading"
+      tipoIcone: "loading",
     };
   }
 
@@ -76,17 +84,15 @@ export default class FormDatabase extends React.Component {
   newModulos = async (data) => {
     const modulos = await listar.getModulo(data);
     if (modulos == "Error") {
-      this.setState({ openIP: true });
-      this.setState({ mensagemIP: "Erro na requisição" });
-      this.setState({ tipoIcone: "error" });
+      this.setState({erro: true})
     } else {
       this.setState({ modulos });
       db.transaction((tx) => {
         tx.executeSql("DELETE FROM modulo");
         tx.executeSql(
-          "INSERT INTO modulo (modelo, descricao, potencia, area, eficiencia, peso, garantia1, garantia2 )" +
-            "values (?, ?, ?, ?, ?, ?, ?, ?)",
-          ["Selecione", "Selecione", 0, 0, 0, 0, 0, 0],
+          "INSERT INTO modulo (id, modelo, descricao, potencia, area, eficiencia, peso, garantia1, garantia2 )" +
+            "values (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          [0, "Selecione", "Selecione", 0, 0, 0, 0, 0, 0],
           (txObj, resultSet) =>
             this.setState({
               data: this.state.data.concat({
@@ -105,9 +111,10 @@ export default class FormDatabase extends React.Component {
         );
         for (let x = 0; x < this.state.modulos.length; x++) {
           tx.executeSql(
-            "INSERT INTO modulo (modelo, descricao, potencia, area, eficiencia, peso, garantia1, garantia2 )" +
-              "values (?, ?, ?, ?, ?, ?, ?, ?)",
+            "INSERT INTO modulo (id, modelo, descricao, potencia, area, eficiencia, peso, garantia1, garantia2 )" +
+              "values (?, ?, ?, ?, ?, ?, ?, ?, ?)",
             [
+              this.state.modulos[x].id,
               this.state.modulos[x].modelo,
               this.state.modulos[x].descricao,
               this.state.modulos[x].potencia,
@@ -120,7 +127,7 @@ export default class FormDatabase extends React.Component {
             (txObj, resultSet) =>
               this.setState({
                 data: this.state.data.concat({
-                  id: resultSet.insertId,
+                  id: this.state.modulos[x].id,
                   modelo: this.state.modulos[x].modelo,
                   descricao: this.state.modulos[x].descricao,
                   potencia: this.state.modulos[x].potencia,
@@ -141,18 +148,16 @@ export default class FormDatabase extends React.Component {
     const cidades = await listar.getCidade(this.state.ip);
     const medias = await listar.getMedia(this.state.ip);
     if (cidades == "Error" || medias == "Error") {
-      this.setState({ openIP: true });
-      this.setState({ mensagemIP: "Erro na requisição" });
-      this.setState({ tipoIcone: "error" });
-      return "Error";
+      this.setState({erro: true})
     } else {
       this.setState({ cidades, medias });
       db.transaction((tx) => {
         tx.executeSql("DELETE FROM cidade");
         for (let x = 0; x < this.state.cidades.length; x++) {
           tx.executeSql(
-            "INSERT INTO cidade (nome, cep, media)" + "values (?, ?, ?)",
+            "INSERT INTO cidade (id, nome, cep, media)" + "values (?, ?, ?, ?)",
             [
+              this.state.cidades[x].id,
               this.state.cidades[x].nome,
               this.state.cidades[x].cep,
               this.state.medias[x].media,
@@ -160,7 +165,7 @@ export default class FormDatabase extends React.Component {
             (txObj, resultSet) =>
               this.setState({
                 data: this.state.data.concat({
-                  id: resultSet.insertId,
+                  id: this.state.cidades[x].id,
                   nome: this.state.cidades[x].nome,
                   cep: this.state.cidades[x].cep,
                   media: this.state.medias[x].media,
@@ -176,21 +181,19 @@ export default class FormDatabase extends React.Component {
   newTarifas = async () => {
     const tarifas = await listar.getTarifa(this.state.ip);
     if (tarifas == "Error") {
-      this.setState({ openIP: true });
-      this.setState({ mensagemIP: "Erro na requisição" });
-      this.setState({ tipoIcone: "error" });
+      this.setState({erro: true})
     } else {
       this.setState({ tarifas });
       db.transaction((tx) => {
         tx.executeSql("DELETE FROM tarifa");
         for (let x = 0; x < this.state.tarifas.length; x++) {
           tx.executeSql(
-            "INSERT INTO tarifa (valor)" + "values (?)",
-            [this.state.tarifas[x].valor],
+            "INSERT INTO tarifa (id, valor)" + "values (?, ?)",
+            [this.state.tarifas[x].id, this.state.tarifas[x].valor],
             (txObj, resultSet) =>
               this.setState({
                 data: this.state.data.concat({
-                  id: resultSet.insertId,
+                  id: this.state.tarifas[x].id,
                   valor: this.state.tarifas[x].valor,
                 }),
               }),
@@ -204,17 +207,15 @@ export default class FormDatabase extends React.Component {
   newPadroes_Entrada = async () => {
     const tipoRede = await listar.getTipoRede(this.state.ip);
     if (tipoRede == "Error") {
-      this.setState({ openIP: true });
-      this.setState({ mensagemIP: "Erro ao atualizar dados" });
-      this.setState({ tipoIcone: "error" });
+      this.setState({erro: true})
     } else {
       this.setState({ tipoRede });
       db.transaction((tx) => {
         tx.executeSql("DELETE FROM padroes_entrada");
         tx.executeSql(
-          "INSERT INTO padroes_entrada (descricao, minimo, coluna1, solo)" +
-            "values (?, ?, ?, ?)",
-          ["Selecione", 0, 0, 0],
+          "INSERT INTO padroes_entrada (id, descricao, minimo, coluna1, solo)" +
+            "values (?, ?, ?, ?, ?)",
+          [0 ,"Selecione", 0, 0, 0],
           (txObj, resultSet) =>
             this.setState({
               data: this.state.data.concat({
@@ -229,9 +230,10 @@ export default class FormDatabase extends React.Component {
         );
         for (let x = 0; x < this.state.tipoRede.length; x++) {
           tx.executeSql(
-            "INSERT INTO padroes_entrada (descricao, minimo, coluna1, solo)" +
-              "values (?, ?, ?, ?)",
+            "INSERT INTO padroes_entrada (id, descricao, minimo, coluna1, solo)" +
+              "values (?, ?, ?, ?, ?)",
             [
+              this.state.tipoRede[x].id,
               this.state.tipoRede[x].descricao,
               this.state.tipoRede[x].minimo,
               this.state.tipoRede[x].coluna1,
@@ -240,7 +242,7 @@ export default class FormDatabase extends React.Component {
             (txObj, resultSet) =>
               this.setState({
                 data: this.state.data.concat({
-                  id: resultSet.insertId,
+                  id: this.state.tipoRede[x].id,
                   descricao: this.state.tipoRede[x].descricao,
                   minimo: this.state.tipoRede[x].minimo,
                   coluna1: this.state.tipoRede[x].coluna1,
@@ -257,17 +259,15 @@ export default class FormDatabase extends React.Component {
   newDisjuntores = async () => {
     const Disjuntores = await listar.getDisjuntor(this.state.ip);
     if (Disjuntores == "Error") {
-      this.setState({ openIP: true });
-      this.setState({ mensagemIP: "Valide o IP do servidor !!" });
-      this.setState({ tipoIcone: "error" });
+      this.setState({erro: true})
     } else {
       this.setState({ Disjuntores });
       db.transaction((tx) => {
         tx.executeSql("DELETE FROM disj_entrada");
         tx.executeSql(
-          "INSERT INTO disj_entrada (descricao, demanda, amper, codPadrao)" +
-            "values (?, ?, ?, ?)",
-          ["Selecione", 0, 0, 0],
+          "INSERT INTO disj_entrada (id, descricao, demanda, amper, codPadrao)" +
+            "values (?, ?, ?, ?, ?)",
+          [0, "Selecione", 0, 0, 0],
           (txObj, resultSet) =>
             this.setState({
               data: this.state.data.concat({
@@ -282,22 +282,23 @@ export default class FormDatabase extends React.Component {
         );
         for (let x = 0; x < this.state.Disjuntores.length; x++) {
           tx.executeSql(
-            "INSERT INTO disj_entrada (descricao, demanda, amper, amper)" +
-              "values (?, ?, ?, ?)",
+            "INSERT INTO disj_entrada (id, descricao, demanda, amper, codPadrao)" +
+              "values (?, ?, ?, ?, ?)",
             [
+              this.state.Disjuntores[x].id,
               this.state.Disjuntores[x].descricao,
               this.state.Disjuntores[x].demanda,
               this.state.Disjuntores[x].amper,
-              this.state.Disjuntores[x].codPadraoId,
+              this.state.Disjuntores[x].cod_padrao.id,
             ],
             (txObj, resultSet) =>
               this.setState({
                 data: this.state.data.concat({
-                  id: resultSet.insertId,
+                  id: this.state.Disjuntores[x].id,
                   descricao: this.state.Disjuntores[x].descricao,
                   demanda: this.state.Disjuntores[x].demanda,
                   amper: this.state.Disjuntores[x].amper,
-                  codPadrao: this.state.Disjuntores[x].codPadraoId,
+                  codPadrao: this.state.Disjuntores[x].cod_padrao.id,
                 }),
               }),
             (txObj, error) => console.log("Error", error)
@@ -309,16 +310,14 @@ export default class FormDatabase extends React.Component {
   newTipoInstall = async () => {
     const tipoInstall = await listar.getTipoInstall(this.state.ip);
     if (tipoInstall == "Error") {
-      this.setState({ openIP: true });
-      this.setState({ mensagemIP: "Verifique se você está online!" });
-      this.setState({ tipoIcone: "error" });
+      this.setState({erro: true})
     } else {
       this.setState({ tipoInstall });
       db.transaction((tx) => {
         tx.executeSql("DELETE FROM tipo_instalacao");
         tx.executeSql(
-          "INSERT INTO tipo_instalacao (descricao)" + "values (?)",
-          ["Selecione"],
+          "INSERT INTO tipo_instalacao (id, descricao)" + "values (?, ?)",
+          [0, "Selecione"],
           (txObj, resultSet) =>
             this.setState({
               data: this.state.data.concat({
@@ -330,13 +329,51 @@ export default class FormDatabase extends React.Component {
         );
         for (let x = 0; x < this.state.tipoInstall.length; x++) {
           tx.executeSql(
-            "INSERT INTO tipo_instalacao (descricao)" + "values (?)",
-            [this.state.tipoInstall[x].descricao],
+            "INSERT INTO tipo_instalacao (id, descricao)" + "values (?, ?)",
+            [this.state.tipoInstall[x].id , this.state.tipoInstall[x].descricao],
             (txObj, resultSet) =>
               this.setState({
                 data: this.state.data.concat({
-                  id: resultSet.insertId,
+                  id: this.state.tipoInstall[x].id,
                   descricao: this.state.tipoInstall[x].descricao,
+                }),
+              }),
+            (txObj, error) => console.log("Error", error)
+          );
+        }
+      });
+    }
+  };
+  newCalculo = async () => {
+    const CalculoKWP = await listar.getCalculoKWP(this.state.ip);
+    if (CalculoKWP == "Error") {
+      this.setState({erro: true})
+    } else {
+      this.setState({ CalculoKWP });
+      console.log(this.state.CalculoKWP.length)
+      db.transaction((tx) => {
+        tx.executeSql("DELETE FROM calculo_kwp");
+        for (let x = 0; x < this.state.CalculoKWP.length; x++) {
+          tx.executeSql(
+            "INSERT INTO calculo_kwp (id, POTEN1, POTEN2, TELHADO, SOLO, codPadrao)" +
+              "values (?, ?, ?, ?, ?, ?)",
+            [
+              this.state.CalculoKWP[x].id,
+              this.state.CalculoKWP[x].POTEN1,
+              this.state.CalculoKWP[x].POTEN2,
+              this.state.CalculoKWP[x].TELHADO,
+              this.state.CalculoKWP[x].SOLO,
+              this.state.CalculoKWP[x].cod_padrao.id,
+            ],
+            (txObj, resultSet) =>
+              this.setState({
+                data: this.state.data.concat({
+                  id: this.state.CalculoKWP[x].id,
+                  POTEN1: this.state.CalculoKWP[x].POTEN1,
+                  POTEN2: this.state.CalculoKWP[x].POTEN2,
+                  TELHADO: this.state.CalculoKWP[x].TELHADO,
+                  SOLO: this.state.CalculoKWP[x].SOLO,
+                  codPadrao: this.state.CalculoKWP[x].cod_padrao.id,
                 }),
               }),
             (txObj, error) => console.log("Error", error)
@@ -350,46 +387,59 @@ export default class FormDatabase extends React.Component {
       this.setState({ mensagem: "Informe um Ip" });
       this.openModal(true);
     } else {
-      if (this.newCidades() == "Error") {
-        this.setState({ openIP: false });
+
+      setTimeout(() => {
+        this.setState({ mensagemIP: "Carregando.." });
+        this.setState({ tipoIcone: "loading" });
+        this.newModulos(data.ip);
+        this.newCidades();
+        console.log("Erro:" + this.state.erro)
+        this.setState({erro:true})
+      }, 1000);
+      if (this.state.erro) {
+        console.log("IF")
+        this.setState({ mensagemIP: "Erro na requisição" });
+        this.setState({ tipoIcone: "error" });
+        this.openModal(false);
       } else {
         this.newCidades();
         this.setState({ openIP: true });
         this.setState({ mensagemIP: "Atualizando Tabela Cidades" });
-      }
-      setTimeout(() => {
-        this.newModulos(data.ip);
-        this.setState({ mensagemIP: "Atualizando Tabela Modulos" });
         setTimeout(() => {
-          this.newTarifas();
-          this.setState({ mensagemIP: "Atualizando Tabela Tarifas" });
+          this.newModulos(data.ip);
+          this.setState({ mensagemIP: "Atualizando Tabela Modulos" });
           setTimeout(() => {
-            this.newPadroes_Entrada();
-            this.setState({ mensagemIP: "Atualizando Tabela Tipo de Rede" });
+            this.newTarifas();
+            this.setState({ mensagemIP: "Atualizando Tabela Tarifas" });
             setTimeout(() => {
-              this.newDisjuntores();
-              this.setState({
-                mensagemIP: "Atualizando Tabela Disjuntores Padrão",
-              });
+              this.newPadroes_Entrada();
+              this.setState({ mensagemIP: "Atualizando Tabela Tipo de Rede" });
               setTimeout(() => {
-                this.newTipoInstall();
+                this.newDisjuntores();
+                this.newCalculo();
                 this.setState({
-                  mensagemIP: "Atualizando Tabela Tipo Instalação",
+                  mensagemIP: "Atualizando Tabela Disjuntores Padrão",
                 });
                 setTimeout(() => {
+                  this.newTipoInstall();
                   this.setState({
-                    mensagemIP: "Concluído com Sucesso!!",
-                    tipoIcone:"sucess"
+                    mensagemIP: "Atualizando Tabela Tipo Instalação",
                   });
                   setTimeout(() => {
-                    this.setState({ openIP: false });
+                    this.setState({
+                      mensagemIP: "Concluído com Sucesso!!",
+                      tipoIcone: "sucess",
+                    });
+                    setTimeout(() => {
+                      this.setState({ openIP: false });
+                    }, 1000);
                   }, 1000);
-                }, 1000);
-              }, 4000);
-            }, 2000);
+                }, 4000);
+              }, 2000);
+            }, 1000);
           }, 1000);
-        }, 1000);
-      }, 20000);
+        }, 20000);
+      }
     }
   };
 
@@ -433,8 +483,12 @@ export default class FormDatabase extends React.Component {
           mensagem={this.state.mensagem}
           open={this.state.open}
           execute={this.openModal}
-        />        
-        <ModalIP open={this.state.openIP} mensagem={this.state.mensagemIP} tipoIcone={this.state.tipoIcone} />
+        />
+        <ModalIP
+          open={this.state.openIP}
+          mensagem={this.state.mensagemIP}
+          tipoIcone={this.state.tipoIcone}
+        />
       </View>
     );
   }
