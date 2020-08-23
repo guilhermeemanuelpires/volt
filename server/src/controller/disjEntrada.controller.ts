3cc88a5d0a5e755855885099db9d7f3e7428095f
douglas-cezaro <cezarodouglas@gmail.com>
2020-08-23 16:05:55 -0300

    Ajustes server

--- server/src/controller/disjEntrada.controller.ts
+++ server/src/controller/disjEntrada.controller.ts
@@ -2,16 +2,16 @@ import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import { DisjuntorEntity } from "../entity/disjEntrada.entity";
 class DisjutorController {
-
-    public async DisjutorController(req: Request, res: Response) {
-        try {
-            const Disjutores = await getRepository(DisjuntorEntity).find();
-            return res.status(200).send({Disjutores});
-        } catch (error) {
-            res.status(500).send(error);
-        }
+  public async DisjutorController(req: Request, res: Response) {
+    try {
+      const Disjutores = await getRepository(DisjuntorEntity).find({
+        relations: ["cod_padrao"],
+      });
+      return res.status(200).send({ Disjutores });
+    } catch (error) {
+      res.status(500).send(error);
     }
-
+  }
 }
 
 export default new DisjutorController();
