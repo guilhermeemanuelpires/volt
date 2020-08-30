c7da6c56c5b882d97badcba3a50d3763472d09d2
Guilherme Pires <guilhermeemanuel45@gmail.com>
2020-08-30 11:09:11 -0300

    Ajuste title botão

--- client/src/AppContainer.js
+++ client/src/AppContainer.js
@@ -5988,7 +5988,7 @@ export default function AppContainer(props) {
                 <Button
                   disabled={!!loadingKey}
                   isLoading={loadingKey === key}
-                  title="Gerar PDF"
+                  title="Salvar"
                   onPress={onButtonPress(key, action)}
                 />
               </View>
